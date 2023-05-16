5ba64bfe39148eeb96146301daae64abcf9efb89
kristanhilby <kmhilby@mit.edu>
2023-05-16 12:31:04 -0400

    Add aerodynamic forces

--- src/rigid3d_airplane.py
+++ src/rigid3d_airplane.py
@@ -6,6 +6,11 @@ import os
 import polyscope as ps
 import polyscope.imgui as psim
 
+import scipy.constants
+
+from scipy.spatial.transform import Rotation
+
+
 try:
     import igl
 finally:
@@ -153,33 +158,45 @@ class Aircraft:
         '''
 
         system_def = {}
-
+        system = Aircraft()
 
         # Chosen default parameters:
-        system_def['system_name'] = "aircraft"
-        system_def['problem_name'] = str(problem_name)
+        system.system_name = "Aircraft"
+        system.problem_name = str(problem_name)
+        
         system_def['cond_params'] = jnp.zeros((0,)) # a length-0 array
         system_def['external_forces'] = {}
+        system_def["contact_stiffness"] = 1000000.0
+        system.cond_dim = 0
+        system.body_ID = None
 
         bodies = []
         joint_list = []
         numBodiesFixed = 0
 
         if problem_name == 'generic airplane':
-            # Add all the necessary bodies
-            bodies.append( make_body( os.path.join(".", "data", ".obj")))
+            scale = 1
             
+            # Add all the necessary bodies
+            bodies.append( make_body( os.path.join(".", "data", "Glider.obj"), 1000, scale))
+            
+            numBodiesFixed = 0
+
             # Add all the necessary joints
-            joint_list.append( make_joint(0, -1, bodies, jnp.array([ 0,           0.08      ,0.044 ]), jnp.array([ 0, 0.0, 1.0 ]) ))
+            #joint_list.append( make_joint(0, -1, bodies, jnp.array([ 0, 0.08 ,0.044 ]), jnp.array([ 0, 0.0, 1.0 ]) ))
 
             # Define the external forces
-            system_def["gravity"] = jnp.array([0.0, -0.98, 0.0])
-            system_def['external_forces']['']
+            system_def["gravity"] = jnp.array([0.0, 0.0, -9.8])
+            system_def['external_forces']['aero_force'] = 0
+            system_def['external_forces']['thrust_force'] = 0
+            system_def['external_forces']['thrustforce_strength_minmax'] = (0,300)
 
-            config_dim = 280
-            system_def['dim'] = config_dim
-            system_def['init_pos'] = jnp.zeros(config_dim) # some values
-            system_def['interesting_states'] = jnp.zeros((0,config_dim))
+            system.body_ID = np.array([1])
+
+            #config_dim = 280
+            #system_def['dim'] = config_dim
+            #system_def['init_pos'] = jnp.zeros(config_dim) # some values
+            #system_def['interesting_states'] = jnp.zeros((0,config_dim))
         
         elif problem_name == 'stoprotor':
 
@@ -192,8 +209,29 @@ class Aircraft:
         else:
             raise ValueError("could not parse problem name: " + str(problem_name))
 
+        posFixed  = jnp.array( np.array([ body['x0']   for body in bodies[0:numBodiesFixed] ]).flatten() )
+        pos  = jnp.array( np.array([ body['x0']   for body in bodies[numBodiesFixed:] ]).flatten() )
 
-        return system_def
+        mass = jnp.array( np.array([ body['mass'] for body in bodies[numBodiesFixed:] ]).flatten() )
+        
+        #
+        system.dim = pos.size
+
+        system.bodiesRen = bodies
+        system.n_bodies = len(bodies)
+        
+        #
+        system.joints = joint_list
+
+        system_def['fixed_pos'] = posFixed
+        system_def['rest_pos'] = pos
+        system_def['init_pos'] = pos
+        system_def['mass'] = mass
+        system_def['dim'] = pos.size
+
+        system_def['interesting_states'] = system_def['init_pos'][None,:]
+
+        return system, system_def
   
     # ===========================================
     # === Energy functions 
@@ -201,14 +239,43 @@ class Aircraft:
 
     # These define the core physics of our system
 
-    def potential_energy(system_def, q):
+    def potential_energy(self, system_def, q):
         # TODO implement
-        return 0.
+        qR = q.reshape(-1,4,3)
+        massR = system_def['mass'].reshape(-1,4,4)
+        gravity = system_def["gravity"]
+        c_weighted = massR[:,3,3][:,None]*qR[:,3,:]
+        gravity_energy = -jnp.sum(c_weighted * gravity[None,:])
+        return gravity_energy
    
+    def calculate_thrust_energy(self, thrust_force, distance):
+        # Assuming constant thrust force and distance in the direction of the force
 
-    def kinetic_energy(system_def, q, q_dot):
-        # TODO implement
-        return 0.
+        # Calculate work done
+        work_done = thrust_force * distance
+
+        # Energy is equal to the work done
+        energy = work_done
+
+        return energy
+    
+    def kinetic_energy(self, system_def, q, q_dot):
+        q_dotR = q_dot.reshape(-1,4,3)
+        massR = system_def['mass'].reshape(-1,4,4)
+        
+        A = jnp.swapaxes(q_dotR,1,2) @ massR @ q_dotR
+        Ke_motion = 0.5*jnp.sum(jnp.trace(A, axis1=1, axis2=2))
+
+        thrust_force =  system_def['external_forces']['thrust_force']
+        distance = distance.append(q)
+        speed = speed.append(q_dot)
+        displacement = np.subtract(distance[-1], distance[-2])
+        speed_change = np.subtract(speed[-1], speed[-2])
+        Ke_thrust = self.calculate_thrust_energy(thrust_force, displacement)
+        
+        aerodynamic_force = self.compute_aerodynamic_forces(self.bodies)
+        ke_aero = self.aero_energy(aerodynamic_force, displacement, self.mass, speed[-2], speed[-1])
+        return np.add(np.add(Ke_motion,Ke_thrust),ke_aero)
 
     # ===========================================
     # === Conditional systems
@@ -220,29 +287,118 @@ class Aircraft:
         # TODO implement
         return jnp.zeros((0,))
 
+    def aero_energy(self, aerodynamic_force, displacement, mass, initial_velocity, final_velocity):
+        # Calculate the work done
+        work = np.dot(aerodynamic_force, displacement)
+
+        # Calculate the change in kinetic energy
+        delta_kinetic_energy = 0.5 * mass * (final_velocity**2 - initial_velocity**2)
+
+        # Total energy is the sum of work and change in kinetic energy
+        energy = work + delta_kinetic_energy
+
+        return energy
+
+
+    def compute_aerodynamic_forces(self, bodies):
+        fluid_density = scipy.constants.density_of_air  # Density of air
+        wind_velocity = jnp.array([0.0, 0.0, 0.0])  # Example wind velocity (modify as needed)
+        
+        air_density = 0.5 * fluid_density * wind_velocity.dot(wind_velocity)
+
+        for body in bodies:
+            v = body['v']
+            f = body['f']
+            W = body['W']
+            x0 = body['x0']
+
+            # Compute the relative velocity between the body and the wind
+            body_velocity = v.dot(W)
+            relative_velocity = body_velocity - wind_velocity
+
+            # Compute the surface area of each triangle face
+            face_normals = utils.triangle_normals(v, f)
+            face_areas = utils.triangle_areas(v, f)
+            surface_areas = face_areas.dot(jnp.abs(face_normals))
+
+            # Compute the angle of attack for each face
+            face_rotations = Rotation.align_vectors(jnp.array([0, 0, 1]), face_normals)
+            face_rotations_angles = face_rotations[1].magnitude()
+
+            # Compute the aerodynamic forces for each face
+            face_forces = jnp.zeros_like(v)
+            for i, face in enumerate(f):
+                face_normal = face_normals[i]
+                face_area = surface_areas[i]
+                face_rotation_angle = face_rotations_angles[i]
+
+                # Compute the lift and drag coefficients based on the angle of attack
+                lift_coefficient = self.compute_lift_coefficient(face_rotation_angle)
+                drag_coefficient = self.compute_drag_coefficient(face_rotation_angle)
+
+                # Compute the lift and drag forces
+                lift_force = 0.5 * air_density * face_area * lift_coefficient * jnp.abs(relative_velocity).dot(face_normal) * face_normal
+                drag_force = 0.5 * air_density * face_area * drag_coefficient * relative_velocity.dot(face_normal) * face_normal
+
+                # Accumulate the forces for each vertex
+                for vertex_index in face:
+                    face_forces[vertex_index] += lift_force + drag_force
+
+            # Convert the face forces to vertex forces
+            vertex_forces = face_forces / W
+
+            # Apply the aerodynamic forces to the body
+            body['external_forces'] += vertex_forces
+
+
+
     # ===========================================
     # === Visualization routines
     # ===========================================
-    
+
     def build_system_ui(system_def):
-        # Construct a Polyscope gui to tweak parameters of the system
-        # Make psim.InputFloat etc calls here
+        if psim.TreeNode("system UI"):
+            psim.TextUnformatted("External forces:")
 
-        # If appliciable, the cond_params values and external_forces values should be 
-        # made editable in this function.
+            if "thrust_force" in system_def["external_forces"]:
+                low, high = system_def['external_forces']['thrustforce_strength_minmax']
+                _, new_val = psim.SliderFloat("thrust_force", float(system_def['external_forces'][ 'thrust_force']), low, high)
+                system_def['external_forces']['thrust_force'] = jnp.array(new_val)
 
-        pass
+            if "force_strength_y" in system_def["external_forces"]:
+                low, high = system_def['external_forces']['force_strength_minmax']
+                _, new_val = psim.SliderFloat("force_strength_y", float(system_def['external_forces'][ 'force_strength_y']), low, high)
+                system_def['external_forces']['force_strength_y'] = jnp.array(new_val)
 
-    def visualize(system_def, q):
-        # Create and/or update a Polyscope visualization of the system in its current state
+            if "force_strength_z" in system_def["external_forces"]:
+                low, high = system_def['external_forces']['force_strength_minmax']
+                _, new_val = psim.SliderFloat("force_strength_z", float(system_def['external_forces'][ 'force_strength_z']), low, high)
+                system_def['external_forces']['force_strength_z'] = jnp.array(new_val)
 
-        # TODO implement
-        pass
+
+            psim.TreePop()
+
+    def visualize(self, system_def, x, name="rigid3d", prefix='', transparency=1.):
+
+        xr = jnp.concatenate((system_def['fixed_pos'],x)).reshape(-1,4,3)
+
+        for bid in range(self.n_bodies):
+            v = np.array(jnp.matmul(self.bodiesRen[bid]['W'], xr[bid]))
+            f = np.array(self.bodiesRen[bid]['f'])
+
+            ps_body = ps.register_surface_mesh("body" + prefix + str(bid), v, f)
+            if transparency < 1.:
+                ps_body.set_transparency(transparency)
+
+            transform = np.identity(4)
+            ps_body.set_transform( transform )
+        
+        return ps_body # not clear that anything needs to be returned
     
 
     def visualize_set_nice_view(system_def, q):
         # Set a Polyscope camera view which nicely looks at the scene
-        
+        ps.look_at((1.5, 1.5, 1.5), (0., -.2, 0.))
         # Example:
         # (could also be dependent on x)
         # ps.look_at((2., 1., 2.), (0., 0., 0.))
